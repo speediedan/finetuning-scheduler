ce4f44acdbb741317370c23ed9c70b131d36540e
Daniel Dale <danny.dale@gmail.com>
2022-03-29 12:28:26 -0700

    augment example tests as part of release 0.1 for base pl 1.6.0rc1

--- fts_examples/test_examples.py
+++ fts_examples/test_examples.py
@@ -10,8 +10,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 import os.path
+import re
 
 import pytest
+from packaging.version import Version
+from pkg_resources import get_distribution
 
 from fts_examples import _HF_AVAILABLE
 from tests.helpers.runif import RunIf
@@ -26,6 +29,22 @@ ARGS_DEFAULT = (
     "--data.batch_size 32 "
 )
 ARGS_GPU = ARGS_DEFAULT + "--trainer.gpus 1 "
+EXPECTED_WARNS = [
+    "does not have many workers",
+    "is smaller than the logging interval",
+    "torch.distributed._sharded_tensor will be deprecated",
+    "`np.object` is a deprecated alias",
+    "`np.int` is a deprecated alias",
+]
+MIN_VERSION_WARNS = "1.8"
+MAX_VERSION_WARNS = "1.11"
+# version-specific warns go here
+EXPECTED_VERSION_WARNS = {MIN_VERSION_WARNS: [], MAX_VERSION_WARNS: []}
+torch_version = get_distribution("torch").version
+if Version(torch_version) < Version(MAX_VERSION_WARNS):
+    EXPECTED_WARNS.extend(EXPECTED_VERSION_WARNS[MIN_VERSION_WARNS])
+else:
+    EXPECTED_WARNS.extend(EXPECTED_VERSION_WARNS[MAX_VERSION_WARNS])
 
 
 @pytest.mark.skipif(not _HF_AVAILABLE, reason="Hugging Face transformers and datasets packages required")
@@ -35,7 +54,7 @@ ARGS_GPU = ARGS_DEFAULT + "--trainer.gpus 1 "
     ["nofts_baseline.yaml", "fts_explicit.yaml", "fts_implicit.yaml"],
     ids=["nofts_baseline", "fts_explicit", "fts_implicit"],
 )
-def test_examples_fts_superglue(monkeypatch, tmpdir, config_file):
+def test_examples_fts_superglue(monkeypatch, recwarn, tmpdir, config_file):
     from fts_examples.fts_superglue import cli_main
 
     example_script = os.path.join(os.path.dirname(__file__), "fts_superglue.py")
@@ -48,3 +67,6 @@ def test_examples_fts_superglue(monkeypatch, tmpdir, config_file):
     ]
     monkeypatch.setattr("sys.argv", [example_script, "fit", "--config"] + config_loc + cli_args)
     cli_main()
+    # ensure no unexpected warnings detected
+    matched = [any([re.compile(w).search(w_msg.message.args[0]) for w in EXPECTED_WARNS]) for w_msg in recwarn.list]
+    assert all(matched)
