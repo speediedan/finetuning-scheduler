cd52b78155a15b11fa0d5e9e26cf34fb5348f522
Daniel Dale <danny.dale@gmail.com>
2022-04-07 17:24:29 -0700

    0.1.0 release

--- finetuning_scheduler/__about__.py
+++ finetuning_scheduler/__about__.py
@@ -1,7 +1,7 @@
 import time
 
 _this_year = time.strftime("%Y")
-__version__ = "0.2.0dev"
+__version__ = "0.1.0"
 __author__ = "Dan Dale"
 __author_email__ = "danny.dale@gmail.com"
 __license__ = "Apache-2.0"
