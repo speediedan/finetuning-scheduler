2b7ba9bd561b0cb53459044e4f40d15b5f783ad3
Daniel Dale <danny.dale@gmail.com>
2022-08-06 11:37:29 -0700

    bump dev version

--- src/finetuning_scheduler/__about__.py
+++ src/finetuning_scheduler/__about__.py
@@ -1,7 +1,7 @@
 import time
 
 _this_year = time.strftime("%Y")
-__version__ = "0.2.0rc0"
+__version__ = "0.3.0dev"
 __author__ = "Dan Dale"
 __author_email__ = "danny.dale@gmail.com"
 __license__ = "Apache-2.0"
