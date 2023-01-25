3a2dd6b5afce1f8178524c8cb7dd9ec9089e801c
Daniel Dale <danny.dale@gmail.com>
2023-01-24 17:33:32 -0800

    update requirements and setup for new 0.4.x release branch

--- setup.py
+++ setup.py
@@ -91,10 +91,10 @@ setup(
     keywords=["deep learning", "pytorch", "AI", "machine learning", "pytorch lightning", "fine-tuning", "finetuning"],
     python_requires=">=3.7",
     setup_requires=[],
-    # install_requires=setup_tools._load_requirements(_PATH_REQUIRE),
-    install_requires=setup_tools._load_requirements(
-        _PATH_REQUIRE, pl_commit="fc195b95405e9e2629466e5b28c6a9243209d596"
-    ),
+    install_requires=setup_tools._load_requirements(_PATH_REQUIRE),
+    # install_requires=setup_tools._load_requirements(
+    #     _PATH_REQUIRE, pl_commit="fc195b95405e9e2629466e5b28c6a9243209d596"
+    # ),
     extras_require=extras,
     project_urls={
         "Bug Tracker": "https://github.com/speediedan/finetuning-scheduler/issues",
