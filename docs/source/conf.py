8deddafcbfed51cba4cea88e1fa593011c08a331
Daniel Dale <danny.dale@gmail.com>
2024-10-11 10:45:41 -0700

    add examples requirement for docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -298,6 +298,7 @@ if SPHINX_MOCK_REQUIREMENTS:
     MOCK_PACKAGES += ["torchmetrics"]
     # mock also base packages when we are on RTD since we don't install them there
     MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, "requirements", "base.txt"))
+    MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, "requirements", "examples.txt"))
     MOCK_PACKAGES += package_list_from_file(os.path.join(PATH_ROOT, "requirements", "extra.txt"))
 MOCK_PACKAGES = [PACKAGE_MAPPING.get(pkg, pkg) for pkg in MOCK_PACKAGES]
 autodoc_mock_imports = MOCK_PACKAGES
