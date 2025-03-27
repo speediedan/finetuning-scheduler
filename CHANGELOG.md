ccd82c6abd019a92a3cde96471de60357f5f3f4a
Daniel Dale <danny.dale@gmail.com>
2025-03-27 07:15:35 -0700

    added yaml (multi)representer for PretrainedConfig object types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/).
 
+## [2.5.1] - 2025-03-27
+
+### Added
+
+- Support for Lightning ``2.5.1``
+- added (multi)representer for ``PretrainedConfig`` object types
+
 ## [2.5.0] - 2024-12-20
 
 ### Added
--- setup.py
+++ setup.py
@@ -131,15 +131,15 @@ def _setup_args(standalone: bool = False) -> Dict[str, Any]:
     )
 
     base_reqs = "standalone_base.txt" if standalone else "base.txt"
-    # install_requires = setup_tools._load_requirements(
-    # _INSTALL_PATHS["require"], file_name=base_reqs, standalone=standalone
-    # )
     install_requires = setup_tools._load_requirements(
-        _INSTALL_PATHS["require"],
-        file_name=base_reqs,
-        standalone=standalone,
-        pl_commit="878ecf56b06d5ae3f482e146e78accabc685bfc7",
+    _INSTALL_PATHS["require"], file_name=base_reqs, standalone=standalone
     )
+    # install_requires = setup_tools._load_requirements(
+    #     _INSTALL_PATHS["require"],
+    #     file_name=base_reqs,
+    #     standalone=standalone,
+    #     pl_commit="878ecf56b06d5ae3f482e146e78accabc685bfc7",
+    # )
     base_setup["install_requires"] = install_requires
     return base_setup
 
